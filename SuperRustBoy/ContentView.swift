e14894b67e4cae37887693efd7fd23e43bdd9950
SeanAsby <sean@movi.ai>
2019-12-13 21:52:43 +0100

    Initial Commit

---
+++ SuperRustBoy/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SuperRustBoy
+//
+//  Created by Sean Inge Asbjørnsen on 13/12/2019.
+//  Copyright © 2019 Sean Inge Asbjørnsen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
