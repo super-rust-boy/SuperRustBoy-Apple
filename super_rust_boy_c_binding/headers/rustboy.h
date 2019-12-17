86c5223511552c6fb1adc65db6e63ae10d08515f
SeanAsby <seanasbjornsen@gmail.com>
2019-12-18 00:02:44 +0100

    Added C-bindings

---
+++ super_rust_boy_c_binding/headers/rustboy.h
@@ -0,0 +1,37 @@
+#define DUTY_12_5 (0 << 6)
+
+#define DUTY_25 (1 << 6)
+
+#define DUTY_50 (2 << 6)
+
+#define DUTY_75 (3 << 6)
+
+#define FRAME_CYCLE (144 * H_CYCLES)
+
+#define FREQ_MAX 131072.0
+
+#define FREQ_MOD 2048
+
+#define H_CYCLES 456
+
+#define JOYPAD 96
+
+#define LCD_STAT 72
+
+#define MODE_1 (154 * H_CYCLES)
+
+#define MODE_2 80
+
+#define MODE_3 (MODE_2 + 168)
+
+#define SERIAL 88
+
+#define TIMER 80
+
+#define V_BLANK 64
+
+typedef struct SGBPalette SGBPalette;
+
+
+
+
---
+++ super_rust_boy_c_binding/src/lib.rs
@@ -0,0 +1,42 @@
+use std::ffi::{c_void, CStr};
+use std::os::raw::c_char;
+use rustboy::{RustBoy, UserPalette, VulkanRenderer, WindowType};
+use winit::EventsLoop;
+
+#[no_mangle]
+pub extern fn rustBoyCreate(cartridge_path: *const c_char, save_file_path: *const c_char) -> *const c_void {
+
+	if cartridge_path.is_null() {
+		println!("Cartridge path is null");
+		return std::ptr::null()
+	}
+
+	let cart_path_result = unsafe { CStr::from_ptr(cartridge_path) };
+	let cart_path = match cart_path_result.to_str() {
+		Ok(c) => c,
+		Err(_) => {
+			println!("Failed to parse cartridge path");
+			return std::ptr::null()
+		}
+	};
+
+	if save_file_path.is_null() {
+		println!("Save file path is null");
+		return std::ptr::null()
+	}
+
+	let save_path_result = unsafe { CStr::from_ptr(save_file_path) };
+	let save_path = match save_path_result.to_str() {
+		Ok(c) => c,
+		Err(_) => {
+			println!("Failed to parse save file path");
+			return std::ptr::null()
+		}
+	};
+
+	let events_loop = EventsLoop::new();
+	let renderer = VulkanRenderer::new(WindowType::Winit(&events_loop));
+	let instance = RustBoy::new(cart_path, save_path, UserPalette::Default, false, renderer);
+
+	Box::into_raw(instance) as *const c_void
+}
