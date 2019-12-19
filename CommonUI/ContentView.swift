8a70865b52bebc6e1528132a2f07dff4c7239443
SeanAsby <seanasbjornsen@gmail.com>
2019-12-19 18:18:56 +0100

    Added iOS target

--- CommonUI/ContentView.swift
+++ CommonUI/ContentView.swift
@@ -33,6 +33,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-			.frame(width: 300, height: 500)
     }
 }
