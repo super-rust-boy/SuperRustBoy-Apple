e40dd73e885987d03b49bc634bb52a1413828bd8
Sean Inge Asbjørnsen <SeanAsby@users.noreply.github.com>
2019-12-19 14:39:30 +0100

    C-bindings to super rust boy for use from Swift (#1)
    * Added C-bindings
    
    * Xcode building with C binding crate
    
    * Added empty delete method
    
    * Added button API
    
    * Added calls to button API
    
    * Added button convert func
    
    * From raw in delete

--- SuperRustBoy/RustBoy.swift
+++ SuperRustBoy/RustBoy.swift
@@ -9,12 +9,40 @@
 import RustBoy
 
 internal class RustBoy {
+
+	internal enum Button {
+		case left, right, up, down, a, b, start, select
+
+		fileprivate var asCoreButton: rustBoyButton {
+			switch self {
+				case .left:		return rustBoyButtonLeft
+				case .right:	return rustBoyButtonRight
+				case .up:		return rustBoyButtonUp
+				case .down:		return rustBoyButtonDown
+				case .a:		return rustBoyButtonA
+				case .b:		return rustBoyButtonB
+				case .start:	return rustBoyButtonStart
+				case .select:	return rustBoyButtonSelect
+			}
+		}
+	}
+
 	internal init?(cartridgePath: String, saveFilePath: String) {
-
 		guard let ref = rustBoyCreate(cartridgePath, saveFilePath) else { return nil }
-
 		coreRef = ref
 	}
 
+	internal func buttonDown(_ button: Button) {
+		rustBoyButtonClickDown(coreRef, button.asCoreButton)
+	}
+
+	internal func buttonUp(_ button: Button) {
+		rustBoyButtonClickUp(coreRef, button.asCoreButton)
+	}
+
 	private let coreRef: UnsafeRawPointer
+
+	deinit {
+		rustBoyDelete(coreRef)
+	}
 }
---
+++ super_rust_boy_c_binding/src/lib.rs
@@ -0,0 +1,87 @@
+use std::ffi::{c_void, CStr};
+use std::os::raw::c_char;
+use rustboy::{RustBoy, UserPalette, VulkanRenderer, WindowType, Buttons};
+use winit::EventsLoop;
+
+#[repr(C)]
+pub enum rustBoyButton {
+	rustBoyButtonLeft,
+	rustBoyButtonRight,
+	rustBoyButtonUp,
+	rustBoyButtonDown,
+	rustBoyButtonA,
+	rustBoyButtonB,
+	rustBoyButtonStart,
+	rustBoyButtonSelect
+}
+
+#[no_mangle]
+pub extern fn rustBoyCreate(cartridge_path: *const c_char, save_file_path: *const c_char) -> *const c_void {
+
+	if cartridge_path.is_null() {
+		println!("Cartridge path is null");
+		return std::ptr::null()
+	}
+
+	let cart_path_result = unsafe { CStr::from_ptr(cartridge_path) };
+	let cart_path = match cart_path_result.to_str() {
+		Ok(c) => c,
+		Err(_) => {
+			println!("Failed to parse cartridge path");
+			return std::ptr::null()
+		}
+	};
+
+	if save_file_path.is_null() {
+		println!("Save file path is null");
+		return std::ptr::null()
+	}
+
+	let save_path_result = unsafe { CStr::from_ptr(save_file_path) };
+	let save_path = match save_path_result.to_str() {
+		Ok(c) => c,
+		Err(_) => {
+			println!("Failed to parse save file path");
+			return std::ptr::null()
+		}
+	};
+
+	let events_loop = EventsLoop::new();
+	let renderer = VulkanRenderer::new(WindowType::Winit(&events_loop));
+	let instance = RustBoy::new(cart_path, save_path, UserPalette::Default, false, renderer);
+
+	Box::into_raw(instance) as *const c_void
+}
+
+#[no_mangle]
+pub extern fn rustBoyDelete(instance: *const c_void) {
+	let _ = unsafe { Box::from_raw(instance as *mut RustBoy) };
+}
+
+#[no_mangle]
+pub extern fn rustBoyButtonClickDown(instance: *const c_void, button: rustBoyButton) {
+	let mut rust_boy = unsafe { Box::from_raw(instance as *mut RustBoy) };
+	rust_boy.set_button(button_for_rust_boy_button(button), true);
+	Box::into_raw(rust_boy);
+}
+
+#[no_mangle]
+pub extern fn rustBoyButtonClickUp(instance: *const c_void, button: rustBoyButton) {
+	let mut rust_boy = unsafe { Box::from_raw(instance as *mut RustBoy) };
+	rust_boy.set_button(button_for_rust_boy_button(button), false);
+	Box::into_raw(rust_boy);
+}
+
+fn button_for_rust_boy_button(button: rustBoyButton) -> Buttons {
+	match button {
+		rustBoyButton::rustBoyButtonLeft => Buttons::A,
+		rustBoyButton::rustBoyButtonRight => Buttons::A,
+		rustBoyButton::rustBoyButtonUp => Buttons::A,
+		rustBoyButton::rustBoyButtonDown => Buttons::A,
+
+		rustBoyButton::rustBoyButtonA => Buttons::A,
+		rustBoyButton::rustBoyButtonB => Buttons::B,
+		rustBoyButton::rustBoyButtonStart => Buttons::START,
+		rustBoyButton::rustBoyButtonSelect => Buttons::SELECT
+	}
+}
