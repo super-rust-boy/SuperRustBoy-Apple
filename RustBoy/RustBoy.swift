ed0ffc499bc4ece646fb98b793b9597c602b2b2e
Sean Inge Asbjørnsen <sean.inge.asbjornsen@rikstv.no>
2020-02-05 20:10:57 +0100

    Clean up RustBoy

--- RustBoy/RustBoy.swift
+++ RustBoy/RustBoy.swift
@@ -36,62 +36,27 @@ internal class RustBoy {
 		}
 	}
 
-	internal var display: DisplayView?
+    internal var display: DisplayView? {
+        didSet {
+            coreRustBoy?.display = display
+        }
+    }
 
 	internal init() {}
 
     internal func buttonDown(_ button: ButtonType) {
 #if os(OSX)
-        coreRustBoy?.buttonDown(button)
+        coreRustBoy?.buttonDown(rustBoyButton(button))
 #endif
     }
 
 	internal func buttonUp(_ button: ButtonType) {
 #if os(OSX)
-        coreRustBoy?.buttonUp(button)
+        coreRustBoy?.buttonUp(rustBoyButton(button))
 #endif
-    }
-
-#if os(OSX)
-    private class CoreRustBoy {
-
-        fileprivate weak var display: DisplayView?
-
-        fileprivate init?(cartridge: Cartridge) {
-            guard let coreRef = rustBoyCreate(cartridge.path, cartridge.saveFilePath) else { return nil }
-            self.coreRef = coreRef
-            timer = Timer.scheduledTimer(withTimeInterval: 1 / RustBoy.framerate, repeats: true) { timer in
-                rustBoyFrame(coreRef, &self.buffer, UInt32(self.buffer.count))
-
-                let data = Data(bytes: &self.buffer, count: self.buffer.count)
-                let image = NSImage(data: data)
-
-                self.display?.image = image
-            }
-        }
-
-        fileprivate func buttonDown(_ button: ButtonType) {
-            rustBoyButtonClickDown(coreRef, rustBoyButton(button))
-        }
-
-        fileprivate func buttonUp(_ button: ButtonType) {
-            rustBoyButtonClickUp(coreRef, rustBoyButton(button))
-        }
-
-        deinit {
-            timer?.invalidate()
-            rustBoyDelete(coreRef)
-        }
-
-        private let coreRef: UnsafeRawPointer
-        private var timer: Timer?
-        private var buffer = [UInt32](repeating: 0, count: 144 * 160)
     }
 
     private var coreRustBoy: CoreRustBoy?
-#endif
-
-    private static let framerate: Double = 60
 
 	private func boot() -> BootStatus {
 		guard let cart = cartridge else { return .cartridgeMissing }
@@ -99,11 +64,72 @@ internal class RustBoy {
         guard let coreRustBoy = CoreRustBoy(cartridge: cart) else { return .failedToInitCore }
 
         self.coreRustBoy = coreRustBoy
+        self.coreRustBoy?.display = display
 #endif
 
 		return .success
 	}
 }
+
+#if os(OSX)
+fileprivate class CoreRustBoy {
+
+    fileprivate weak var display: DisplayView?
+
+    fileprivate init?(cartridge: RustBoy.Cartridge) {
+        guard let coreRef = rustBoyCreate(cartridge.path, cartridge.saveFilePath) else { return nil }
+        self.coreRef = coreRef
+        timer = Timer.scheduledTimer(withTimeInterval: 1 / Self.framerate, repeats: true) { timer in
+            self.render()
+        }
+    }
+
+    fileprivate func buttonDown(_ button: rustBoyButton) {
+        rustBoyButtonClickDown(coreRef, button)
+    }
+
+    fileprivate func buttonUp(_ button: rustBoyButton) {
+        rustBoyButtonClickUp(coreRef, button)
+    }
+
+    private func render() {
+        rustBoyFrame(coreRef, &buffer, UInt32(buffer.count))
+
+        guard let display = display else { return }
+
+        let data = Data(bytes: &buffer, count: buffer.count)
+        let dataProvider = CGDataProvider(data: data as CFData)!
+        let colorSpace = CGColorSpace(name: CGColorSpace.sRGB)!
+
+        let coreImage = CGImage(
+            width:              160,
+            height:             144,
+            bitsPerComponent:   8,
+            bitsPerPixel:       32,
+            bytesPerRow:        640,
+            space:              colorSpace,
+            bitmapInfo:         [CGBitmapInfo.byteOrder32Big, CGBitmapInfo(rawValue: CGImageAlphaInfo.noneSkipLast.rawValue)],
+            provider:           dataProvider,
+            decode:             nil,
+            shouldInterpolate:  false,
+            intent:             CGColorRenderingIntent.saturation
+        )
+
+        let image = NSImage(cgImage: coreImage!, size: NSSize(width: 160, height: 144))
+        display.image = image
+    }
+
+    deinit {
+        timer?.invalidate()
+        rustBoyDelete(coreRef)
+    }
+
+    private let coreRef: UnsafeRawPointer
+    private var timer: Timer?
+    private var buffer = [UInt32](repeating: 0, count: 144 * 640)
+    private static let framerate: Double = 60
+}
+#endif
 
 #if os(OSX)
     private extension rustBoyButton {
