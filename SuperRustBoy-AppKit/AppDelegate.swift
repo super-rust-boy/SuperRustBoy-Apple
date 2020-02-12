63808b9582ce829f0ed7e06735faa57105c1bf90
Sean Inge Asbjørnsen <sean.inge.asbjornsen@rikstv.no>
2020-02-12 18:38:55 +0100

    Using spaces

--- SuperRustBoy-AppKit/AppDelegate.swift
+++ SuperRustBoy-AppKit/AppDelegate.swift
@@ -12,38 +12,37 @@ import SwiftUI
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-	var window: NSWindow!
+    var window: NSWindow!
 
     private let rustBoy = RustBoy.setup {
         $0.autoBoot = true
     }
 
-	func applicationDidFinishLaunching(_ aNotification: Notification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
 
-		let cartPath = NSHomeDirectory() + "/Roms/PokemonRed.gb"
+        let cartPath = NSHomeDirectory() + "/Roms/PokemonRed.gb"
         let savePath = NSHomeDirectory() + "/Roms/SaveFiles/PokemonRed.sav"
 
         rustBoy.cartridge = RustBoy.Cartridge(path: cartPath, saveFilePath: savePath)
 
-		// Create the SwiftUI view that provides the window contents.
-		let rootView = RustBoyView(rustBoy: rustBoy)
+        // Create the SwiftUI view that provides the window contents.
+        let rootView = RustBoyView(rustBoy: rustBoy)
 
-		// Create the window and set the content view.
-		window = NSWindow(
-		    contentRect:	NSRect(x: 0, y: 0, width: 300, height: 600),
-		    styleMask:		[.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-		    backing:		.buffered,
-			defer:			false
-		)
-		window.center()
-		window.setFrameAutosaveName("Main Window")
-		window.contentView = NSHostingView(rootView: rootView)
-		window.makeKeyAndOrderFront(nil)
-	}
+        // Create the window and set the content view.
+        window = NSWindow(
+            contentRect:    NSRect(x: 0, y: 0, width: 300, height: 600),
+            styleMask:        [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing:        .buffered,
+            defer:            false
+        )
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: rootView)
+        window.makeKeyAndOrderFront(nil)
+    }
 
-	func applicationWillTerminate(_ aNotification: Notification) {
-		// Insert code here to tear down your application
-	}
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
 
 }
-
